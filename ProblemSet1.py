cb1dc254aa39a38c031bae131e2c286c2541e09e
MWasilczyk <michael@wasilczyk.com>
2015-06-21 11:11:37 -0400

    PS1 Correct

---
+++ ProblemSet1.py
@@ -0,0 +1,11 @@
+# Write a program that counts up the number of vowels contained in the string s.
+
+s = 'a slightly different type of string'
+vowels = 'aeiouAEIOU'
+num_vowels = 0
+
+for letter in s:
+    if letter in vowels:
+        num_vowels += 1
+
+print "Number of vowels: %r" % num_vowels
