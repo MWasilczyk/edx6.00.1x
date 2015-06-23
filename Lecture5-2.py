0cfb6ce5158938bd0177957f87db3128850bd62d
MWasilczyk <michael@wasilczyk.com>
2015-06-23 19:30:34 -0400

    Problem 2 Update

--- Lecture5-2.py
+++ Lecture5-2.py
@@ -13,8 +13,6 @@ print x*(x**(y-1))
 def recurPower(base, exp):
     if exp == 0:
         return 1
-    elif exp == 1:
-        return base
     return base * recurPower(base, exp - 1)
 
 print recurPower(x,y)
