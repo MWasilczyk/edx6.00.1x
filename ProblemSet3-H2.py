4ff415b083b80b5a44bef53a56b0f185c220d57e
Michael Wasilczyk <michael@wasilczyk.com>
2015-06-25 20:54:00 -0400

    Saved and Correct

---
+++ ProblemSet3-H2.py
@@ -0,0 +1,20 @@
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    # FILL IN YOUR CODE HERE...
+    i = ''
+    for x in secretWord:
+        if x not in lettersGuessed:
+            i += ' _ '
+        else:
+            i += x
+    return i
+    
+
+secretWord = 'apple' 
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+print getGuessedWord(secretWord, lettersGuessed)
