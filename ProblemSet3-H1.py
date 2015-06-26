13a45349502ffc68bc158a74cef8abd00ef2d6a6
Michael Wasilczyk <michael@wasilczyk.com>
2015-06-25 20:48:40 -0400

    Saved and Correct

---
+++ ProblemSet3-H1.py
@@ -0,0 +1,17 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    for x in secretWord:
+        if x not in lettersGuessed:
+            return False
+    return True
+
+
+secretWord = 'apple' 
+lettersGuessed = []
+print isWordGuessed(secretWord, lettersGuessed)
